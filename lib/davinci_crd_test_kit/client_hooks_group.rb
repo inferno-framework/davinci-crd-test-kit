0b5c17838a6fcf1f56687eb8cc660a2b653d4d37
Stephen MacVicar <smacvicar@mitre.org>
2024-09-11 08:45:38 -0400

    extract client appointment book group

--- lib/davinci_crd_test_kit/client_hooks_group.rb
+++ lib/davinci_crd_test_kit/client_hooks_group.rb
@@ -1,4 +1,4 @@
-require_relative 'client_tests/appointment_book_receive_request_test'
+require_relative 'client_tests/client_appointment_book_group'
 require_relative 'client_tests/client_display_cards_attest'
 require_relative 'client_tests/decode_auth_token_test'
 require_relative 'client_tests/encounter_discharge_receive_request_test'
@@ -57,62 +57,8 @@ module DaVinciCRDTestKit
           description: 'The `iss` claim of the JWT in the Authorization header ' \
                        'will be used to associate incoming requests with this test session'
 
-    group do
-      title 'appointment-book'
-      description <<~DESCRIPTION
-        The appointment-book hook is invoked when the user is scheduling one or more future encounters/visits for the
-        patient. These tests are based on the following criteria:
-          * [CRD IG requirements for this hook](https://hl7.org/fhir/us/davinci-crd/STU2/hooks.html#appointment-book),
-          which include the profiles that are expected to be used for the resources resolved to by `context` FHIR ID
-          fields
-          * Specific [appointment-book `context` requirements](https://cds-hooks.hl7.org/hooks/appointment-book/2023SepSTU1Ballot/appointment-book/)
-          defined in the CDS Hooks specification
-
-        This version of the CRD implementation guide refers to version 1.0 of the hook.
-      DESCRIPTION
-
-      optional
-      run_as_group
-
-      config(
-        inputs: {
-          auth_token_headers_json: { name: :appointment_book_auth_token_headers_json },
-          auth_tokens: { name: :appointment_book_auth_tokens },
-          auth_tokens_jwk_json: { name: :appointment_book_auth_tokens_jwk_json },
-          client_access_token: { name: :appointment_book_client_access_token },
-          client_fhir_server: { name: :appointment_book_client_fhir_server },
-          crd_jwks_keys_json: { name: :appointment_book_crd_jwks_keys_json },
-          custom_response: { name: :appointment_book_custom_response },
-          selected_response_types: { name: :appointment_book_selected_response_types }
-        },
-        outputs: {
-          auth_token_headers_json: { name: :appointment_book_auth_token_headers_json },
-          auth_token_payloads_json: { name: :appointment_book_auth_token_payloads_json },
-          auth_tokens: { name: :appointment_book_auth_tokens },
-          auth_tokens_jwk_json: { name: :appointment_book_auth_tokens_jwk_json },
-          client_access_token: { name: :appointment_book_client_access_token },
-          client_fhir_server: { name: :appointment_book_client_fhir_server },
-          crd_jwks_json: { name: :appointment_book_crd_jwks_json },
-          crd_jwks_keys_json: { name: :appointment_book_crd_jwks_keys_json }
-        },
-        options: {
-          hook_name: 'appointment-book',
-          hook_path: APPOINTMENT_BOOK_PATH
-        }
-      )
-
-      test from: :crd_submitted_response_validation
-      test from: :crd_appointment_book_request
-      test from: :crd_decode_auth_token
-      test from: :crd_retrieve_jwks
-      test from: :crd_token_header
-      test from: :crd_token_payload
-      test from: :crd_hook_request_required_fields
-      test from: :crd_hook_request_optional_fields
-      test from: :crd_hook_request_valid_context
-      test from: :crd_hook_request_valid_prefetch
-      test from: :crd_card_display_attest_test
-    end
+    group from: :crd_client_appointment_book,
+          optional: true
 
     group do
       title 'encounter-start'
