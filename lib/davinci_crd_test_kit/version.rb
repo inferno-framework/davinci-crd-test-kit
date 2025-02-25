9c5aaa78d149e919139b5f6e77519d79b3bf31f0
Vanessa Fotso <vfotso@mitre.org>
2025-02-25 17:36:13 -0500

    bump inferno core
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/davinci_crd_test_kit/version.rb
+++ lib/davinci_crd_test_kit/version.rb
@@ -1,4 +1,4 @@
 module DaVinciCRDTestKit
   VERSION = '0.11.0'.freeze
-  LAST_UPDATED = '2025-02-11'.freeze # TODO: update next release
+  LAST_UPDATED = '2025-02-25'.freeze # TODO: update next release
 end
