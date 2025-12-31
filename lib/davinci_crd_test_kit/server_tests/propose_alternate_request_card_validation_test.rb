547509f9e822a66fd61266cd49d0a27d7c0fa367
Karl Naden <karl.naden@gmail.com>
2025-12-30 23:05:47 -0500

    separate out CRD card type identification logic

--- lib/davinci_crd_test_kit/server_tests/propose_alternate_request_card_validation_test.rb
+++ lib/davinci_crd_test_kit/server_tests/propose_alternate_request_card_validation_test.rb
@@ -1,12 +1,14 @@
 require_relative '../test_helper'
 require_relative '../suggestion_actions_validation'
 require_relative '../server_hook_helper'
+require_relative '../cards_identification'
 
 module DaVinciCRDTestKit
   class ProposeAlternateRequestCardValidationTest < Inferno::Test
     include DaVinciCRDTestKit::TestHelper
     include DaVinciCRDTestKit::SuggestionActionsValidation
     include DaVinciCRDTestKit::ServerHookHelper
+    include DaVinciCRDTestKit::CardsIdentification
 
     title 'Valid Propose Alternate Request cards received'
     id :crd_propose_alternate_request_card_validation
@@ -20,33 +22,12 @@ module DaVinciCRDTestKit
     optional
     input :valid_cards_with_suggestions, :contexts
 
-    EXPECTED_RESOURCE_TYPES = %w[
-      Device DeviceRequest Encounter Medication
-      MedicationRequest NutritionOrder ServiceRequest
-      VisionPrescription
-    ].freeze
-
-    def check_action_type(actions, action_type)
-      actions&.any? do |action|
-        action['type'] == action_type && action_resource_type_check(action, EXPECTED_RESOURCE_TYPES)
-      end
-    end
-
-    def propose_alternate_request_card?(card)
-      card['suggestions'].any? do |suggestion|
-        actions = suggestion['actions']
-        has_update = check_action_type(actions, 'update')
-        has_delete = check_action_type(actions, 'delete')
-        has_create = check_action_type(actions, 'create')
-
-        has_update || (has_delete && has_create)
-      end
-    end
-
     run do
       parsed_cards = parse_json(valid_cards_with_suggestions)
       parsed_contexts = parse_json(contexts)
-      proposed_alternate_cards = parsed_cards.filter { |card| propose_alternate_request_card?(card) }
+      proposed_alternate_cards = parsed_cards.filter do |card|
+        propose_alternative_request_response_type?(card)
+      end
 
       skip_if proposed_alternate_cards.blank?,
               "#{tested_hook_name} hook response does not contain a Propose Alternate Request card."
