f7086c0f670a4202de2986bca830228b9718dfa3
Karl Naden <86612928+karlnaden@users.noreply.github.com>
2026-04-28 21:55:36 -0400

    ID-25: Import US Core tests in CRD Client v2.2 tests (#57)
    * import us core tests with 3 version options, default inputs from cds hook request fields
    
    * typo
    
    * update preset

--- lib/davinci_crd_test_kit/client/crd_client_options.rb
+++ lib/davinci_crd_test_kit/client/crd_client_options.rb
@@ -5,5 +5,13 @@ module DaVinciCRDTestKit
 
     SMART_1_REQUIREMENT = { smart_app_launch_version: SMART_1 }.freeze
     SMART_2_REQUIREMENT = { smart_app_launch_version: SMART_2 }.freeze
+
+    US_CORE_3 = 'us_core_3'.freeze
+    US_CORE_6 = 'us_core_6'.freeze
+    US_CORE_7 = 'us_core_7'.freeze
+
+    US_CORE_3_REQUIREMENT = { us_core_version: US_CORE_3 }.freeze
+    US_CORE_6_REQUIREMENT = { us_core_version: US_CORE_6 }.freeze
+    US_CORE_7_REQUIREMENT = { us_core_version: US_CORE_7 }.freeze
   end
 end
