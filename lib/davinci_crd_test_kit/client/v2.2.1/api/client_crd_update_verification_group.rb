fecbe18f1c0f1f7de2d9a888ae643f1e8be63098
Karl Naden <karl@glenviewassociates.com>
2026-05-05 20:48:43 -0400

    cov-info update test

---
+++ lib/davinci_crd_test_kit/client/v2.2.1/api/client_crd_update_verification_group.rb
@@ -0,0 +1,17 @@
+require_relative 'client_coverage_info_update_test'
+
+module DaVinciCRDTestKit
+  module V221
+    class ClientCRDUpdateVerificationGroup < Inferno::TestGroup
+      id :crd_v221_client_update_verification
+      title 'CRD Update Tests'
+      description %(
+        This CRD-specific group verifies that the client was able to
+        persist updates provided by the payer server.
+      )
+      run_as_group
+
+      test from: :crd_v221_client_coverage_info_update
+    end
+  end
+end
