1b5d53bb6204bcd24202b055b9877269bd27c43b
Stephen MacVicar <smacvicar@mitre.org>
2026-05-06 09:38:41 -0400

    clean up code

--- lib/davinci_crd_test_kit/server/v2.2.1/verify_response/order_dispatch_coverage_information_test.rb
+++ lib/davinci_crd_test_kit/server/v2.2.1/verify_response/order_dispatch_coverage_information_test.rb
@@ -13,35 +13,37 @@ module DaVinciCRDTestKit
         reference.resource_type == resource.resourceType && reference.reference_id == resource.id
       end
 
-      def order_resources(hook_call_body) # rubocop:disable Metrics/CyclomaticComplexity
+      def mock_ehr_bundle_resources
+        FHIR.from_contents(mock_ehr_bundle)
+          .entry
+          .map(&:resource)
+      end
+
+      def prefetch_resources(hook_call_body)
+        (hook_call_body['prefetch'] || {})
+          .values
+          .compact
+          .map { |resource_hash| FHIR.from_contents(resource_hash.to_json) }
+          .flat_map do |resource|
+          if resource.is_a? FHIR::Bundle
+            resource.entry.map(&:resource)
+          else
+            resource
+          end
+        end
+      end
+
+      def order_resources(hook_call_body)
         references =
           hook_call_body
             .dig('context', 'dispatchedOrders')
             .map { |reference| FHIR::Reference.new(reference:) }
 
-        bundle = FHIR.from_contents(mock_ehr_bundle)
-
         resources =
-          bundle.entry
-            .map(&:resource)
+          (mock_ehr_bundle_resources + prefetch_resources(hook_call_body))
             .select do |resource|
               references.any? { |reference| resource_matches_reference?(reference, resource) }
             end
-
-        prefetch_resources =
-          (hook_call_body['prefetch'] || {})
-            .values
-            .compact
-            .map { |resource_hash| FHIR.from_contents(resource_hash.to_json) }
-            .flat_map do |resource|
-              if resource.is_a? FHIR::Bundle
-                resource.entry.map(&:resource)
-              else
-                resource
-              end
-            end
-
-        resources += prefetch_resources
 
         unmatched_references =
           references.reject do |reference|
