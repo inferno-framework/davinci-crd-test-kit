295312bcd96c8df6611e91300ca19e14632ec67b
Karl Naden <karl@glenviewassociates.com>
2026-05-06 15:07:01 -0400

    rename group

---
+++ lib/davinci_crd_test_kit/client/v2.2.1/client_cross_hook_group.rb
@@ -0,0 +1,22 @@
+require_relative 'must_support/client_card_must_support_coverage_information_test'
+require_relative 'must_support/client_location_address_propagation_test'
+
+module DaVinciCRDTestKit
+  module V221
+    class ClientCrossHookGroup < Inferno::TestGroup
+      title 'Cross Hook Verification'
+      id :crd_v221_client_cross_hook
+      description <<~DESCRIPTION
+        This group checks CRD requirements that pertain across all hooks
+        rather than a specific one.
+
+        These tests must be run after the tests in the Hooks group are run.
+      DESCRIPTION
+
+      run_as_group
+
+      test from: :crd_v221_client_card_must_support_coverage_information
+      test from: :crd_v221_client_location_address_propagation
+    end
+  end
+end
--- lib/davinci_crd_test_kit/client/v2.2.1/crd_client_suite.rb
+++ lib/davinci_crd_test_kit/client/v2.2.1/crd_client_suite.rb
@@ -1,6 +1,6 @@
 require_relative 'client_fhir_api_group'
 require_relative 'client_hooks_group'
-require_relative 'client_card_must_support_group'
+require_relative 'client_cross_hook_group'
 require_relative 'client_registration_group'
 require_relative '../endpoints/cds_services_discovery_handler'
 require_relative '../../cross_suite/tags'
@@ -132,7 +132,7 @@ module DaVinciCRDTestKit
 
         group from: :crd_v221_client_registration
         group from: :crd_v221_client_hooks
-        group from: :crd_v221_client_card_must_support
+        group from: :crd_v221_client_cross_hook
       end
 
       group from: :crd_v221_client_fhir_api
