3c64728f31878cb5a53a3081f6b238475847dd11
Karl Naden <karl@glenviewassociates.com>
2026-05-08 13:10:50 -0400

    add uniqueness check test

---
+++ lib/davinci_crd_test_kit/client/v2.2.1/must_support/client_hook_instances_unique_test.rb
@@ -0,0 +1,42 @@
+require_relative '../../tagged_request_load_helper'
+require_relative '../../multi_request_message_helper'
+require_relative '../../../cross_suite/tags'
+
+module DaVinciCRDTestKit
+  module V221
+    class ClientHookInstancesUniqueTest < Inferno::Test
+      include TaggedRequestLoadHelper
+      include MultiRequestMessageHelper
+      title 'Request hookInstance values are globally unique'
+      id :crd_v221_client_hook_instances_unique
+      description <<~DESCRIPTION
+        CDS Hooks requires that the `hookInstance` field of each hook
+        request be globally unique so that it can be used for tracking
+        and auditing.
+
+        This test checks that across all hook invocations performed during these
+        tests, the `hookInstance` field was never reused.
+      DESCRIPTION
+
+      config(
+        options: {
+          crd_test_group: DUPLICATED_HOOK_INSTANCE_TAG
+        }
+      )
+
+      run do
+        duplicated_hook_instance_requests = load_hook_requests
+        reused_values = duplicated_hook_instance_requests.map.with_index do |request, request_index|
+          request_body = parse_json_request_entity(request.request_body, 'Request body', request_index)
+          next nil unless request_body.present?
+
+          request_body['hookInstance']
+        end.compact.uniq
+        reused_values.each { |hook_instance| load_tagged_requests(TagMethods.hook_instance_tag(hook_instance)) }
+
+        assert duplicated_hook_instance_requests.blank?,
+               "Inferno received hook requests that re-used `hookInstance` values: #{reused_values.join(', ')}"
+      end
+    end
+  end
+end
