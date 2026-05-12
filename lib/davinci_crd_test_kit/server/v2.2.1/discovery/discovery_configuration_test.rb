4a173d3a7dc2af8e9deee1c2a1adfba212037ab1
Stephen MacVicar <smacvicar@mitre.org>
2026-05-12 09:27:17 -0400

    verify config options for all hook services

--- lib/davinci_crd_test_kit/server/v2.2.1/discovery/discovery_configuration_test.rb
+++ lib/davinci_crd_test_kit/server/v2.2.1/discovery/discovery_configuration_test.rb
@@ -16,6 +16,18 @@ module DaVinciCRDTestKit
 
       input :cds_services
 
+      def primary_hooks
+        ['appointment-book', 'order-sign', 'order-dispatch']
+      end
+
+      def primary_hook?(hook)
+        primary_hooks.include? hook
+      end
+
+      def required_fields
+        ['code', 'type', 'name', 'description', 'default']
+      end
+
       def verify_unique_values(hook, service)
         unique_fields = ['code', 'name', 'description']
 
@@ -48,16 +60,43 @@ module DaVinciCRDTestKit
         end
       end
 
+      def verify_required_fields(hook, service)
+        service.dig('extension', 'davinci-crd.configuration-options')&.each do |config_option|
+          required_fields.each do |field_name|
+            unless config_option.key?(field_name)
+              add_message(
+                'error',
+                "Hook `#{hook}` service `#{service['id']}` configuration option `#{config_option['code']}` " \
+                "does not contain `#{field_name}` field"
+              )
+              next
+            end
+
+            next if field_name == 'default'
+
+            next if config_option[field_name].is_a? String
+
+            type = config_option[field_name].class
+            add_message(
+              'error',
+              "Expected hook `#{hook}` service `#{service['id']}` configuration option `#{service['code']}` " \
+              "field `#{field_name}` to be a String, but found #{type}"
+            )
+          end
+        end
+      end
+
       run do
         services = JSON.parse(cds_services)['services']
 
         services_by_hook = services.group_by { |service| service['hook'] }
 
         services_by_hook.each do |hook, hook_services|
-          hook_services.each { |service| verify_unique_values(hook, service) }
+          hook_services.each do |service|
+            verify_required_fields(hook, service)
+            verify_unique_values(hook, service)
+          end
         end
-
-        primary_hooks = ['appointment-book', 'order-sign', 'order-dispatch']
 
         primary_hook_services = services_by_hook.slice(*primary_hooks).values.flatten
 
@@ -79,44 +118,18 @@ module DaVinciCRDTestKit
           )
         end
 
-        required_fields = ['code', 'type', 'name', 'description', 'default']
-
         primary_hook_services.each do |service|
           coverage_info_config_found =
             service.dig('extension', 'davinci-crd.configuration-options')&.any? do |config_option|
               config_option['code'] == 'coverage-info'
             end
 
-          unless coverage_info_config_found
-            add_message(
-              'error',
-              "Service `#{service['id']}` does not contain a `coverage-info` configuration option"
-            )
-          end
+          next if coverage_info_config_found
 
-          service.dig('extension', 'davinci-crd.configuration-options')&.each do |config_option|
-            required_fields.each do |field_name|
-              unless config_option.key?(field_name)
-                add_message(
-                  'error',
-                  "Service `#{service['id']}` configuration option `#{config_option['code']}` " \
-                  "does not contain `#{field_name}` field"
-                )
-                next
-              end
-
-              next if field_name == 'default'
-
-              next if config_option[field_name].is_a? String
-
-              type = config_option[field_name].class
-              add_message(
-                'error',
-                "Expected service `#{service['id']}` configuration option `#{service['code']}` " \
-                "field `#{field_name}` to be a String, but found #{type}"
-              )
-            end
-          end
+          add_message(
+            'error',
+            "Service `#{service['id']}` does not contain a `coverage-info` configuration option"
+          )
         end
 
         assert messages.none? { |message| message[:type] == 'error' },
