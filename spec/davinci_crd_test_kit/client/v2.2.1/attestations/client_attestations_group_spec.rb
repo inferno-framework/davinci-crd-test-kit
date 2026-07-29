1b12fe61e1be443211ca97bf7711d49cd41f365b
FlexonyoPizza <LeonDhoska@gmail.com>
2026-07-29 18:02:58 -0400

    syntax error

--- spec/davinci_crd_test_kit/client/v2.2.1/attestations/client_attestations_group_spec.rb
+++ spec/davinci_crd_test_kit/client/v2.2.1/attestations/client_attestations_group_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DaVinciCRDTestKit::V221::CRDClientAttestationsGroup do
   let(:suite_id) { 'crd_client_v221' }
   let(:group) { described_class }
 
-  # Every requirement from the CRD v2.2.1 client attestation set, in spreadsheet order.
+  
   let(:expected_requirements) do
     [
       'hl7.fhir.us.davinci-crd_2.2.1@sec-1',
