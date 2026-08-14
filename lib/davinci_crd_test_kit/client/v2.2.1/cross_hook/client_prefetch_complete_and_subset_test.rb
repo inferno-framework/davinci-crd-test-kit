ce69a1215e3824accb14db69f3f4ff85883ebe1e
Karl Naden <karl@glenviewassociates.com>
2026-08-14 00:21:40 -0400

    additional location for prefetch complete tests

--- lib/davinci_crd_test_kit/client/v2.2.1/cross_hook/client_prefetch_complete_and_subset_test.rb
+++ lib/davinci_crd_test_kit/client/v2.2.1/cross_hook/client_prefetch_complete_and_subset_test.rb
@@ -59,15 +59,27 @@ module DaVinciCRDTestKit
                 'The client did not demonstrate both complete and subset prefetch capability. See Messages for details.'
       end
 
-      def find_completeness_tests
+      def find_completeness_tests # rubocop:disable Metrics/CyclomaticComplexity
+        # minimal error checking - want it to bomb if this search logic fails
         hooks_group = self.class.parent.parent.parent.groups.find do |group|
           group.id.to_s.include?('crd_v221_client_hooks')
         end
-        hooks_group.groups.map do |group|
+        completeness_tests = hooks_group.groups.map do |group|
           group.groups.flat_map(&:tests).find do |test|
             test.id.to_s.include?('crd_v221_hook_request_prefetch_complete')
           end
-        end.compact
+        end
+
+        cross_hooks_interaction_group = self.class.parent.parent.groups.find do |group|
+          group.id.to_s.include?('crd_v221_client_cross_hook_interaction')
+        end
+        cross_hooks_completeness_test =
+          cross_hooks_interaction_group.groups.flat_map(&:tests).find do |test|
+            test.id.to_s.include?('crd_v221_hook_request_prefetch_complete')
+          end
+        completeness_tests << cross_hooks_completeness_test
+
+        completeness_tests.compact
       end
 
       def check_for_demonstration(requests, completeness_tests, target_name)
